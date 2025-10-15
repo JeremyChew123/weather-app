8d6c0635f084e48535e478bb62ab2ea47849e7f2
JeremyChew123 <cwqjeremy@gmail.com>
2025-10-15 19:56:06 +0800

    iniital commit

---
+++ WeatherMan/WeatherManApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManApp.swift
+//  WeatherMan
+//
+//  Created by Jeremy Chew on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
